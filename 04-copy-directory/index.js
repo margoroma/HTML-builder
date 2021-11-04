e979d99de230a80715d7d169cc3108d83f2ccbc6
margoroma <margoroma2010@gmail.com>
2021-11-04 22:43:11 +0300

    feat: add script to copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const pathFolder = path.join(__dirname, 'files');
+const pathCopy = path.join(__dirname, 'files-copy');
 
+function copyDirectory() {
+    fs.stat(pathCopy, function(err) {
+        if (err && err.code === 'ENOENT') {
+            fs.mkdir(pathCopy, (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+        }
+        fs.readdir(pathFolder, (err, files) => {
+            if (err) {
+                throw err;
+            }
+            files.forEach(file => {
+                fs.copyFile(path.join(pathFolder, file), path.join(pathCopy, file), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
+            console.log('Directory is copied');
+        });
+    });
+}
+
+copyDirectory();
