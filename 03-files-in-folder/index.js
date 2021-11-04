c3bbc8b04c480de8f3eecb73191dfada2eb93b70
margoroma <margoroma2010@gmail.com>
2021-11-04 22:29:38 +0300

    fix: clear code

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,5 @@
 const fs=require('fs');
 const path=require('path');
-// const secretFolder=path.join(__dirname, '/secret-folder');
 
 fs.readdir(path.join(__dirname, '/secret-folder'), {withFileTypes: true}, (err, files)=>{
     if(err) throw err;
