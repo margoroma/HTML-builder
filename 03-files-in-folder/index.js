dd75e924f9e9837faf40708519a298e23013f74a
margoroma <margoroma2010@gmail.com>
2021-11-04 22:20:24 +0300

    feat: add script information files

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs=require('fs');
+const path=require('path');
+// const secretFolder=path.join(__dirname, '/secret-folder');
+
+fs.readdir(path.join(__dirname, '/secret-folder'), {withFileTypes: true}, (err, files)=>{
+    if(err) throw err;
+    files.forEach(file => {
+        if (file.isFile()) {          
+            fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+               let nameFile=file.name.slice(0, file.name.indexOf('.'));
+               let extFile=path.extname(file.name).slice(1);
+               console.log(`${nameFile} - ${extFile} - ${stats.size/1000}kb`);
+            });          
+        }      
+    });
+});
